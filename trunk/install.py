b0e19a6cefee4cc70fa26bebc5301f9631e51497
antyqjon <antyqjon@e3e259e2-fbb7-11de-bceb-371847987001>
2010-01-25 18:48:53 +0000

    Modified install script.

--- trunk/install.py
+++ trunk/install.py
@@ -16,9 +16,15 @@ AQUILA_DOC_DIR = os.path.join(AQUILA_DIR, "doc")
 INSTALL_DOC_DIR = os.path.join(INSTALL_DIR, "doc")
 
 if os.name == 'posix':
-	INSTALL_LIB_DIR = "/usr/lib/aquila"
-	INSTALL_SRC_DIR = "/usr/include/aquila"
-	INSTALL_DOC_DIR = "/usr/share/doc/aquila"
+    INSTALL_LIB_DIR = "/usr/lib/aquila"
+    INSTALL_SRC_DIR = "/usr/include/aquila"
+    INSTALL_DOC_DIR = "/usr/share/doc/aquila"
+    
+    for path in (INSTALL_LIB_DIR, INSTALL_DOC_DIR, INSTALL_SRC_DIR):
+        if os.path.exists(path):
+            print "Removing files from %s" % path
+            shutil.rmtree(path)
+        
 else:
     print "Installing from %s to %s" % (AQUILA_DIR, INSTALL_DIR)
 
@@ -34,8 +40,8 @@ shutil.copytree(AQUILA_LIB_DIR, INSTALL_LIB_DIR)
 
 print "Copying include files from %s to %s" % (AQUILA_SRC_DIR, INSTALL_SRC_DIR)
 shutil.copytree(AQUILA_SRC_DIR, INSTALL_SRC_DIR, ignore=shutil.ignore_patterns(
-	'.svn*', '*.cpp', '*.rc', '*.pro*', 'Makefile*', 'debug', 'release',
-	'obj', 'objd', 'object_script.*'))
+    '.svn*', '*.cpp', '*.rc', '*.pro*', 'Makefile*', 'debug', 'release',
+    'obj', 'objd', 'object_script.*'))
 
 print "Copying documentation from %s to %s" % (AQUILA_DOC_DIR, INSTALL_DOC_DIR)
 shutil.copytree(AQUILA_DOC_DIR, INSTALL_DOC_DIR)
