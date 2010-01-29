833c666476d69979d4a8d2317cffa0812dc9be85
antyqjon <antyqjon@e3e259e2-fbb7-11de-bceb-371847987001>
2010-01-29 13:50:01 +0000

    Slightly modified benchmark program.

--- trunk/examples/benchmark/main.cpp
+++ trunk/examples/benchmark/main.cpp
@@ -1,15 +1,10 @@
 #include "Benchmark.h"
 #include <iostream>
-#include <boost/timer.hpp>
 
-int main(int argc, char *argv[])
+int main()
 {
     std::cout << "Benchmarking..." << std::endl;
-    boost::timer t;
-    std::cout << "Minimum elapsed time: " << t.elapsed_min() << std::endl;
-    std::cout << "Maximum elapsed time: " << t.elapsed_max() << std::endl;
-
-    Benchmark b;
+    Benchmark b(100);
     b.run();
 
     std::cout << "Press Enter to leave benchmark...";
