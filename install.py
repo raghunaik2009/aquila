f8b4c905fa5776de7d1e0a38a8d76b1b5d3c8dec
antyqjon <antyqjon@e3e259e2-fbb7-11de-bceb-371847987001>
2010-03-23 14:11:50 +0000

    Reworked the install script.

--- install.py
+++ install.py
@@ -1,48 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+################################################################################
+# DO NOT MODIFY, SETTINGS ARE LOCATED IN cfg_nt.py / cfg_posix.py
+################################################################################
+
 import os
 import shutil
 
-# change the directory below
-INSTALL_DIR = r"F:\aquila"
+try:
+    cfg = __import__('cfg_%s' % os.name)
+except ImportError:
+    print 'Sorry, your system is not supported yet!'
+    import sys
+    sys.exit(1)
+
+for path in (cfg.INSTALL_LIB_DIR, cfg.INSTALL_DOC_DIR, cfg.INSTALL_SRC_DIR):
+    if os.path.exists(path):
+        print "Removing files from %s" % path
+        shutil.rmtree(path)
+
+cfg.prepare()
+
 AQUILA_DIR = os.path.dirname(__file__)
-
 AQUILA_LIB_DIR = os.path.join(AQUILA_DIR, "lib")
-INSTALL_LIB_DIR = os.path.join(INSTALL_DIR, "lib")
 AQUILA_SRC_DIR = os.path.join(AQUILA_DIR, "src")
-INSTALL_SRC_DIR = os.path.join(INSTALL_DIR, r"include\aquila")
 AQUILA_DOC_DIR = os.path.join(AQUILA_DIR, "doc")
-INSTALL_DOC_DIR = os.path.join(INSTALL_DIR, "doc")
-
-if os.name == 'posix':
-    INSTALL_LIB_DIR = "/usr/lib/aquila"
-    INSTALL_SRC_DIR = "/usr/include/aquila"
-    INSTALL_DOC_DIR = "/usr/share/doc/aquila"
-    
-    for path in (INSTALL_LIB_DIR, INSTALL_DOC_DIR, INSTALL_SRC_DIR):
-        if os.path.exists(path):
-            print "Removing files from %s" % path
-            shutil.rmtree(path)
-        
-else:
-    print "Installing from %s to %s" % (AQUILA_DIR, INSTALL_DIR)
-
-    if os.path.exists(INSTALL_DIR):
-        print "Looks like it's gonna be an upgrade..."
-        shutil.rmtree(INSTALL_DIR)
-        
-    os.mkdir(INSTALL_DIR)
-    print "Created install directory"
-	
-ignore = shutil.ignore_patterns('.svn*', '*.cpp', '*.rc', '*.pro*', 'Makefile*', 'debug', 'release',
-								'obj', 'objd', 'object_script.*')
-
-print "Copying libraries from %s to %s" % (AQUILA_LIB_DIR, INSTALL_LIB_DIR)
-shutil.copytree(AQUILA_LIB_DIR, INSTALL_LIB_DIR, ignore=ignore)
-
-print "Copying include files from %s to %s" % (AQUILA_SRC_DIR, INSTALL_SRC_DIR)
-shutil.copytree(AQUILA_SRC_DIR, INSTALL_SRC_DIR, ignore=ignore)
-
-print "Copying documentation from %s to %s" % (AQUILA_DOC_DIR, INSTALL_DOC_DIR)
-shutil.copytree(AQUILA_DOC_DIR, INSTALL_DOC_DIR, ignore=ignore)
+ignore = shutil.ignore_patterns('.svn*', '*.cpp', '*.rc', '*.pro*', 'Makefile*',
+                                'debug', 'release', 'obj', 'objd', 'object_script.*')
+print "Copying libraries from %s to %s" % (AQUILA_LIB_DIR, cfg.INSTALL_LIB_DIR)
+shutil.copytree(AQUILA_LIB_DIR, cfg.INSTALL_LIB_DIR, ignore=ignore)
+print "Copying include files from %s to %s" % (AQUILA_SRC_DIR, cfg.INSTALL_SRC_DIR)
+shutil.copytree(AQUILA_SRC_DIR, cfg.INSTALL_SRC_DIR, ignore=ignore)
+print "Copying documentation from %s to %s" % (AQUILA_DOC_DIR, cfg.INSTALL_DOC_DIR)
+shutil.copytree(AQUILA_DOC_DIR, cfg.INSTALL_DOC_DIR, ignore=ignore)
